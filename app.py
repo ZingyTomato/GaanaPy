251e0d14a81ebccf2b44aeeb74614b52bb4fe397
Zingy Tomato <79736973+ZingyTomato@users.noreply.github.com>
2023-02-02 18:48:23 +0530

    Remove typehints.
    Fixes an issue with newer versions of fastapi due to its new typehint validatoin.

--- app.py
+++ app.py
@@ -7,76 +7,83 @@ app = FastAPI()
 gaanapy = GaanaPy()
 
 @app.get("/")
-async def home() -> dict:
-    return {"Docs": "https://api.nikomusic.tk/docs", "Github": 'https://github.com/ZingyTomato/GaanaPy'}
+async def home():
+    return {"Docs": "/docs", "Github": 'https://github.com/ZingyTomato/GaanaPy'}
 
 @app.get("/songs/search/")
 async def songs_search(query: str = Query(description="Name of the song to search for."), 
-limit: Optional[int] = None) -> dict:
+limit: Optional[int] = None):
     if limit == None:
         limit = 10
     result = await gaanapy.search_songs(query, limit)
     return result
 
 @app.get("/songs/info/")
-async def songs_info(seokey: str = Query(description="The `seokey` of the song. Example: `tyler-herro` in `https://gaana.com/song/tyler-herro`")) -> dict:
+async def songs_info(seokey: str = Query(description=
+                                         "The `seokey` of the song. Example: `tyler-herro` in `https://gaana.com/song/tyler-herro`")):
     track_list = []
     track_list.append(seokey)
     song_info = await gaanapy.get_track_info(track_list)
     return song_info
 
 @app.get("/albums/search/")
-async def albums_search(query: str = Query(description="Name of the album to search for."),
-limit: Optional[int] = None) -> dict:
+async def albums_search(query: str = Query(description=
+                                           "Name of the album to search for."), limit: Optional[int] = None):
     if limit == None:
         limit = 10
     result = await gaanapy.search_albums(query, limit)
     return result
 
 @app.get("/albums/info/")
-async def albums_info(seokey: str = Query(description="The `seokey` of the album. Example: `tyler-herro` in `https://gaana.com/album/tyler-herro`")) -> dict:
+async def albums_info(seokey: str = Query(description=
+                                          "The `seokey` of the album. Example: `tyler-herro` in `https://gaana.com/album/tyler-herro`")):
     album_list = []
     album_list.append(seokey)
     album_info = await gaanapy.get_album_info(album_list, True)
     return album_info
 
 @app.get("/artists/search/")
-async def artists_search(query: str = Query(description="Name of the artist to search for."), limit: Optional[int] = None) -> dict:
+async def artists_search(query: str = Query(description=
+                                            "Name of the artist to search for."), limit: Optional[int] = None):
     if limit == None:
         limit = 10
     result = await gaanapy.search_artists(query, limit)
     return result
 
 @app.get("/artists/info/")
-async def artists_info(seokey: str = Query(description="The `seokey` of the artist. Example, `ksi` in `https://gaana.com/artist/ksi`")) -> dict:
+async def artists_info(seokey: str = Query(description=
+                                           "The `seokey` of the artist. Example, `ksi` in `https://gaana.com/artist/ksi`")) :
     artist_list = []
     artist_list.append(seokey)
     artist_info = await gaanapy.get_artist_info(artist_list, True)
     return artist_info
 
 @app.get("/trending")
-async def get_trending(language: str = Query(description="Available options: English, Hindi, Tamil, Punjabi etc."), limit: Optional[int] = None) -> dict:
+async def get_trending(language: str = Query(description=
+                                             "Available options: English, Hindi, Tamil, Punjabi etc."), limit: Optional[int] = None):
     if limit == None:
         limit = 10
     result = await gaanapy.get_trending(language, limit)
     return result
 
 @app.get("/newreleases")
-async def get_new_releases(language: str = Query(description="Available options: English, Hindi, Tamil, Punjabi etc."), limit: Optional[int] = None) -> dict:
+async def get_new_releases(language: str = Query(description=
+                                                 "Available options: English, Hindi, Tamil, Punjabi etc."), limit: Optional[int] = None):
     if limit == None:
         limit = 10
     result = await gaanapy.get_new_releases(language, limit)
     return result
 
 @app.get("/charts")
-async def get_charts(limit: Optional[int] = None) -> dict:
+async def get_charts(limit: Optional[int] = None):
     if limit == None:
         limit = 10
     result = await gaanapy.get_charts(limit)
     return result
 
 @app.get("/playlists/info/")
-async def playlists_info(seokey: str = Query(description="The `seokey` of the playlist. Example: `gaana-dj-hindi-top-50-1` in `https://gaana.com/playlist/gaana-dj-hindi-top-50-1`")) -> dict:
+async def playlists_info(seokey: str = Query(description=
+                                             "The `seokey` of the playlist. Example: `gaana-dj-hindi-top-50-1` in `https://gaana.com/playlist/gaana-dj-hindi-top-50-1`")):
     playlist_info = await gaanapy.get_playlist_info(seokey)
     return playlist_info
 
